da9a4c13439366099c061feddb2f46d386da5486
K Nishijima <nishijima@r3it.com>
2015-06-16 10:52:27 +0900

    DROP_DOWN対応追加

--- groovy/mkhtml.groovy
+++ groovy/mkhtml.groovy
@@ -11,8 +11,18 @@ def subDomain = config.account.subDomain
 def appId = config.account.appId
 def apiToken = config.account.apiToken
 
+def formHeader = """<form class="kintoneform" id="kintoneform">"""
+def formFooter = """
+  <div id="kintoneFormSubmitSuccess" style="display: none" class="alert alert-success" role="alert"></div>
+  <div id="kintoneFormSubmitAlert" style="display: none" class="alert alert-danger" role="alert"></div>
+  <button type="buton" id="kintoneFormSubmit" class="btn btn-primary">送信</button>
+</form>
+"""
+
 def reqAlert = ' <span class="alert-danger">*</span>'
 def required = 'required="required"'
+
+println formHeader
 
 def http = new HTTPBuilder("https://${subDomain}.cybozu.com")
 
@@ -35,10 +45,25 @@ http.request(Method.GET, ContentType.TEXT) {
           reqAlert = ''
           required = ''
         }
-        // type switch
-        def input = """<input type="text" class="form-control" id="_kintoneform_${code}" name="${code}" value="" maxlength="256" ${required} />"""
-        if (it.type == 'MULTI_LINE_TEXT'){
-          input = """<textarea class="form-control" id="_kintoneform_${code}" name="_kintoneform_${code}" ${required}></textarea>"""
+        
+        def input = ''
+        switch (it.type) {
+          case "MULTI_LINE_TEXT":
+            input = """<textarea class="form-control" id="_kintoneform_${code}" name="_kintoneform_${code}" rows="3" ${required}></textarea>"""
+            required = 'required'
+            break
+        
+          case "DROP_DOWN":
+            input = """<select class="form-control drop_down" id="_kintoneform_${code}" name="_kintoneform_${code}">"""
+            it.options.each { optLabel ->
+              input <<= """<option>${optLabel}</option>"""
+            }
+            input <<= '</select>'
+            break
+            
+          default:
+            input = """<input type="text" class="form-control" id="_kintoneform_${code}" name="${code}" value="" maxlength="256" ${required} />"""
+            break
         }
 
         def template = """
@@ -53,3 +78,5 @@ http.request(Method.GET, ContentType.TEXT) {
     }
   }
 }
+
+println formFooter
